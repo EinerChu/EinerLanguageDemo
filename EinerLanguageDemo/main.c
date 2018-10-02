dfcb54fd4dc825ba6707fd787380fa409ac70599
Einer <einerchu@outlook.com>
2018-10-02 21:33:54 +0800

    code
    first commit data

---
+++ EinerLanguageDemo/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  EinerLanguageDemo
+//
+//  Created by 褚鹏 on 2018/10/2.
+//  Copyright © 2018年 Einer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
